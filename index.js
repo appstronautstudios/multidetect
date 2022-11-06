64166fbc6b4ba6322c367ac1527ef3c1602f29ce
Oskar S <oseparovic@gmail.com>
2022-11-05 17:53:23 -0700

    for each input string now returns a JSON doc for the expected language with confidence for that language.

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 import cld from 'cld';
 import LanguageDetect from 'languagedetect';
-import { franc, francAll } from 'franc';
+import { francAll } from 'franc';
 import getCountryISO2 from 'country-iso-3-to-2';
 
 async function detectSingleViaCLD(title) {
-    // promise resolve is in format {code:ISO, percent:confidence%}
-    // promise reject is error code. Not meeting min confidence is a reject as well
     return new Promise((resolve, reject) => {
-        // language check on titles. We do this in lieu of proper language settings. A lot of streamers
-        // skip over changing their language and mark it as "en" which creates really bad multi-lingual content
         cld.detect(title)
             .then((results) => {
                 if (results != null && results.languages != null && results.languages.length > 0) {
-                    let bestMatch = results.languages[0];
-                    return resolve(bestMatch);
-                }
-                else {
+                    return resolve(results.languages);
+                } else {
                     throw new Error("null results");
                 }
             })
             .catch((e) => {
-                return resolve({ code: "xx", percent: 0 });
+                return resolve([]);
             });
-    });
-}
-
-export async function detectLanguageViaCLD(titles) {
-    return new Promise((resolve, reject) => {
-        let promises = [];
-        for (let title of titles) {
-            promises.push(detectSingleViaCLD(title));
-        }
-        Promise.all(promises).then((results) => {
-            for (let i = 0; i < results.length; i++) {
-                if (results[i] == null) continue;
-                results[i].text = titles[i];
-            }
-            return resolve(results);
-        });
     });
 }
 
@@ -47,71 +25,92 @@ async function detectSingleViaLanguageDetect(title) {
         detector.setLanguageType("iso2");
         let results = detector.detect(title);
         if (results != null && results.length > 0) {
-            let bestMatch = results[0];
-            let iso = bestMatch[0];
-            let confidence = bestMatch[1] * 100;
-            if (iso == null || iso == "null") {
-                throw new Error("confident language is null?");
-            } else {
-                return resolve({
-                    code: iso,
-                    percent: confidence
-                });
+            let output = [];
+            for (let result of results) {
+                let iso = result[0];
+                let confidence = result[1] * 100;
+                if (iso != null && iso != "null") {
+                    output.push({
+                        code: iso,
+                        percent: confidence
+                    });
+                }
             }
+            return resolve(output);
         } else {
-            return resolve(null);
+            return resolve([]);
         }
-    });
-}
-
-export async function detectLanguageViaLanguageDetect(titles) {
-    return new Promise((resolve, reject) => {
-        let promises = [];
-        for (let title of titles) {
-            promises.push(detectSingleViaLanguageDetect(title));
-        }
-        Promise.all(promises).then((results) => {
-            for (let i = 0; i < results.length; i++) {
-                if (results[i] == null) continue;
-                results[i].text = titles[i];
-            }
-            return resolve(results);
-        });
     });
 }
 
 async function detectSingleViaFranc(title) {
     return new Promise((resolve, reject) => {
-        let results = franc(title);
-        let iso = getCountryISO2(results.toUpperCase());
-        if (iso == null || iso == "null") iso = "un";
-        iso = iso.toLowerCase();
-        let confidence = 100;
-        return resolve({
-            code: iso,
-            percent: confidence
-        });
+        let results = francAll(title);
+        if (results != null && results.length > 0) {
+            let output = [];
+            for (let result of results) {
+                let iso = getCountryISO2(result[0].toUpperCase());
+                let confidence = result[1] * 100;
+                if (iso != null && iso != "null") {
+                    output.push({
+                        code: iso.toLowerCase(),
+                        percent: confidence
+                    });
+                }
+            }
+            return resolve(output);
+        } else {
+            return resolve([]);
+        }
     });
 }
 
-export async function detectLanguageViaFranc(titles) {
+/**
+ * goes through inputs and for each one checks if the expected language is detected
+ * above confidence threshold. Uses the method supplied. Returns an 
+ * @param {*} inputs 
+ * @param {*} expected 
+ * @param {*} method 
+ * @returns array of results corresponding to the inputs in the following format [{code:ISO, percent:FLOAT, text:INPUT}, ...]
+ */
+export async function languageConfidence(inputs, expected, method) {
     return new Promise((resolve, reject) => {
         let promises = [];
-        for (let title of titles) {
-            promises.push(detectSingleViaFranc(title));
+        for (let input of inputs) {
+            if (method == "cld") {
+                promises.push(detectSingleViaCLD(input));
+            } else if (method == "ld") {
+                promises.push(detectSingleViaLanguageDetect(input));
+            } else if (method == "franc") {
+                promises.push(detectSingleViaFranc(input));
+            } else {
+                return reject("unexpected method code");
+            }
         }
         Promise.all(promises).then((results) => {
+            let matches = [];
             for (let i = 0; i < results.length; i++) {
-                if (results[i] == null) continue;
-                results[i].text = titles[i];
+                let output = {};
+                let input = inputs[i];
+                let matchSet = results[i];
+                for (let match of matchSet) {
+                    if (match.code == expected) {
+                        output.code = match.code;
+                        output.percent = match.percent;
+                        break;
+                    }
+                }
+                output.text = input;
+                matches.push(output);
             }
-            return resolve(results);
+            return resolve(matches);
         });
     });
 }
 
 async function test(testSet) {
     const DIVIDER = "==========";
+    const THRESHOLD = 75;
     let cloudTranslate = null;
     let cldResults = null;
     let languageDetectResults = null;
@@ -124,23 +123,11 @@ async function test(testSet) {
         console.log(e);
     }
     console.log("trying cld");
-    try {
-        cldResults = await detectLanguageViaCLD(testSet);
-    } catch (e) {
-        console.log(e);
-    }
+    cldResults = await languageConfidence(testSet, "ru", "cld");
     console.log("trying languagedetect");
-    try {
-        languageDetectResults = await detectLanguageViaLanguageDetect(testSet);
-    } catch (e) {
-        console.log(e);
-    }
+    languageDetectResults = await languageConfidence(testSet, "ru", "ld");
     console.log("trying franc");
-    try {
-        francResults = await detectLanguageViaFranc(testSet);
-    } catch (e) {
-        console.log(e);
-    }
+    francResults = await languageConfidence(testSet, "ru", "franc");
     // compile
     console.log(DIVIDER);
     let cloudTranslateSuccesses = 0;
@@ -150,10 +137,10 @@ async function test(testSet) {
     let allSuccesses = 0;
     for (let i = 0; i < testSet.length; i++) {
         console.log(testSet[i]);
-        // let ctSuccess = (cloudTranslate[i].code.startsWith("ru") && cloudTranslate[i].percent > (75));
-        let cldSuccess = (cldResults[i].code.startsWith("ru") && cldResults[i].percent > (75));
-        let ldSuccess = (languageDetectResults[i].code.startsWith("ru") && languageDetectResults[i].percent > (75));
-        let francSuccess = francResults[i].code.startsWith("ru");
+        // let ctSuccess = (cloudTranslate[i].code.startsWith("ru") && cloudTranslate[i].percent > THRESHOLD);
+        let cldSuccess = (cldResults[i].code != null && cldResults[i].percent > THRESHOLD);
+        let ldSuccess = (languageDetectResults[i].code != null && languageDetectResults[i].percent > THRESHOLD);
+        let francSuccess = (francResults[i].code != null && francResults[i].percent > THRESHOLD);
         // console.log("ct success = " + ctSuccess);
         console.log("cld  success = " + cldSuccess);
         console.log("ld success = " + ldSuccess);
@@ -172,25 +159,27 @@ async function test(testSet) {
     console.log("franc rate: " + francSuccesses / testSet.length);
     console.log("all rate: " + allSuccesses / testSet.length);
 }
-test([
-    "ФИДЕРЫ",
-    "Челка мешат",
-    "C 3000 до 6000 ЗА 5 ДНЕЙ ЛИБО НОВАЯ СТРИЖКА (ДЕНЬ4 3500)",
-    "Он уже накопил,алло стример",
-    "ГДЕ Я БЛЯТЬ?",
-    "типичный шейкер в твоей команде",
-    "Фо рил",
-    "ВП - сосалка",
-    "Попытка номер STO",
-    "ВОДУ ПИЛ",
-    "в трон по гофру",
-    "rfhmjкарточку",
-    "чтоооо топ 1 морф задоджил дум...",
-    "Познавательный донат",
-    "roma..",
-    "TORONTOTOKYO?",
-    "понесло",
-    "плачу на техно feat. rainypa1n",
-    "0 помощи блядь НОЛЬ ПОМОЩИИИ!",
-    "обмоханый"
-]);
+
+// test(["Он уже накопил,алло стример"]);
+// test([
+//     "ФИДЕРЫ",
+//     "Челка мешат",
+//     "C 3000 до 6000 ЗА 5 ДНЕЙ ЛИБО НОВАЯ СТРИЖКА (ДЕНЬ4 3500)",
+//     "Он уже накопил,алло стример",
+//     "ГДЕ Я БЛЯТЬ?",
+//     "типичный шейкер в твоей команде",
+//     "Фо рил",
+//     "ВП - сосалка",
+//     "Попытка номер STO",
+//     "ВОДУ ПИЛ",
+//     "в трон по гофру",
+//     "rfhmjкарточку",
+//     "чтоооо топ 1 морф задоджил дум...",
+//     "Познавательный донат",
+//     "roma..",
+//     "TORONTOTOKYO?",
+//     "понесло",
+//     "плачу на техно feat. rainypa1n",
+//     "0 помощи блядь НОЛЬ ПОМОЩИИИ!",
+//     "обмоханый"
+// ]);
